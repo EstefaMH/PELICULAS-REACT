4c7df179e59b72441d2cd8ed7867e0492e76af5d
EstefaMH <98820291+EstefaMH@users.noreply.github.com>
2023-05-19 09:44:29 -0500

    add pages-> Home,Movies,Credits

---
+++ src/Pages/Credits.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CreditsBody from '../Components/Credits/CreditsBody';
+import Navbar from '../Components/Navbar/Navbar';
+import Footer from '../Components/Footer/Footer';
+
+const Credits = () => {
+    return (
+        <div>
+            <Navbar />
+            <CreditsBody />
+            <Footer/>
+        </div>
+    );
+}
+
+export default Credits;
---
+++ src/Pages/Home.js
@@ -0,0 +1,34 @@
+import Footer from "../Components/Footer/Footer";
+import GetAllMovies from "../Services/GetAllMovies";
+import React, { useEffect, useState } from "react";
+import Navbar from "../Components/Navbar/Navbar";
+import GetPopularMovies from "../Services/GetPopularMovies";
+
+function Home() {
+  const [movies, setMovies] = useState();
+
+  useEffect(function () {
+    GetAllMovies()
+      .then(function (res) {
+        console.log(res.data.results);
+        setMovies(res.data.results);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+      GetPopularMovies();
+  }, []);
+
+  
+
+  return (
+    <div>
+      <Navbar/>
+      <Footer />
+
+    </div>
+  );
+}
+
+export default Home;
