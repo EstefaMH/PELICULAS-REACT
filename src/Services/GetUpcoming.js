df7b2b67ec1a9e052729ce21e9d679e46020fc01
EstefaMH <98820291+EstefaMH@users.noreply.github.com>
2023-05-22 10:12:32 -0500

    add service getUpcoming

--- src/Services/GetUpcoming.js
+++ src/Services/GetUpcoming.js
@@ -4,19 +4,18 @@ function GetUpcomming() {
  
     const options = {
     method: "GET",
-    url: "https://api.themoviedb.org/3/movie/upcoming",
+    url: "https://api.themoviedb.org/3/movie/upcoming?api_key=64edd47a5cafe08e6d7d3db82221a5c3",
     params: { language: "es-ES", page: "1" },
     headers: { accept: "application/json" },
   };
 
-  axios
-    .request(options)
-    .then(function (response) {
+  return axios.request(options)
+   /* .then(function (response) {
       console.log(response.data);
     })
     .catch(function (error) {
       console.error(error);
-    });
+    });*/
 }
 
 export default GetUpcomming;
