ea5b931afac2042ffe469bba108805739007bff6
EstefaMH <98820291+EstefaMH@users.noreply.github.com>
2023-05-24 00:46:05 -0500

    update component-> CardsMovies, CardsSearch , Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,67 +1,84 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import GetMovieSearch from "../../Services/GetMovieSearch";
 
 function Navbar() {
-    return (
-        <section id="header">
-            <nav class="navbar navbar-expand-md navbar-light" id="navbar_sticky">
-                <div class="container">
-                    <Link to="/" class="navbar-brand text-white fw-bold" >
-                        <i class="fa fa-video-camera col_red me-1"></i>
-                        YESmovies
-                    </Link>
-                    <button
-                        class="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mb-0">
-                            <li class="nav-item">
-                                <Link to="/" class="nav-link" aria-current="page" >
-                                    Home
-                                </Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link to="/movies" class="nav-link">
-                                    Peliculas
-                                </Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link to="/credits" class="nav-link" >
-                                    Creditos
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <form
-                        class="container d-flex col-md-6 col-sm-12 mt-3 "
-                        action="searchMovie.html"
-                    >
-                        <input
-                            type="search"
-                            class="form-control bg-black w-60"
-                            id="input-search"
-                            autocomplete="autocomplete"
-                            placeholder="Buscar una pelicula"
-                        />
-                        <button
-                            class="btn btn text-white bg_red rounded-0 border-0"
-                            type="submit"
-                            onclick="getMovieSearch(inputSearch())"
-                        >
-                            Buscar
-                        </button>
-                    </form>
-                </div>
-            </nav>
-        </section>
-    );
+  const [search, setSearch] = useState();
+
+  function handleChange(e) {
+    setSearch(e.target.value);
+  }
+
+  console.log(search);
+
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    localStorage.setItem('arrayMovies' , search);
+    window.location.href = '/search';
+   
+};
+
+  return (
+    <section id="header">
+      <nav class="navbar navbar-expand-md navbar-light" id="navbar_sticky">
+        <div class="container">
+          <Link to="/" class="navbar-brand text-white fw-bold">
+            <i class="fa fa-video-camera col_red me-1"></i>
+            YESmovies
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mb-0">
+              <li className="nav-item">
+                <Link to="/" className="nav-link" aria-current="page">
+                  Home
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/movies" className="nav-link">
+                  Peliculas
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/credits" className="nav-link">
+                  Creditos
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <form onSubmit={handleSubmit}  className="container d-flex col-md-6 col-sm-12 mt-3">
+            <input
+              type="search"
+              class="form-control bg-black w-60"
+              id="input-search"
+              autocomplete="autocomplete"
+              placeholder="Buscar una pelicula"
+              onChange={handleChange}
+              value={search}
+            />
+            <button
+              class="btn btn text-white bg_red rounded-0 border-0" 
+            >
+              Buscar
+            </button>
+          </form>
+        </div>
+      </nav>
+    </section>
+  );
 }
 
 export default Navbar;
